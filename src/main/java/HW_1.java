117fc2bd93e48b7f523a4b0057c3696f2fd27f61
Nikpil <pelipenkonikolay@gmail.com>
2021-03-07 16:05:37 +0300

    Домашняя работа 2 Задание 1 добвлено задание 2

--- src/main/java/HW_1.java
+++ src/main/java/HW_1.java
@@ -1,7 +1,20 @@
+import java.util.Arrays;
+
 public class HW_1
 {
     public static void main(String[] args)
     {
+        int[] arr = new int[6];
+        arr[0]=1;
+        arr[1]=1;
+        arr[2]=1;
+        arr[3]=0;
+        arr[4]=0;
+        arr[5]=0;
+       System.out.println(Arrays.toString(arr));
+
+
+
 
 
     }
